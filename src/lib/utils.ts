758b9f55b98c0aa1a0ef6ba1f7605fb0977faf1c
elabdioui <elabdiouihaitam@gmail.com>
2025-09-07 19:40:47 +0100

    fix bugs2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue } from "clsx";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export declare function cn(...inputs: ClassValue[]): string;
+export declare function formatCurrency(amount: number, currency?: string, options?: Intl.NumberFormatOptions): string;
+export declare function generateUniqueId(prefix?: string): string;
+export declare function truncateText(text: string, maxLength: number): string;
+export declare function formatDate(date: Date, options?: Intl.DateTimeFormatOptions): string;
+
+// Fixed: Replace 'any' with proper generic types
+export declare function debounce<T extends (...args: unknown[]) => unknown>(
+  func: T, 
+  wait: number
+): (...args: Parameters<T>) => void;
+
+export declare function throttle<T extends (...args: unknown[]) => unknown>(
+  func: T, 
+  limit: number
+): (...args: Parameters<T>) => void;
